39ebeacf1405e9b32d4fc432ac0288dfb53e6a03
Blisteur <nathan.chiodin@epitech.eu>
2022-08-20 04:18:18 +0200

    [ADD] protected config

--- src/config.php
+++ src/config.php
@@ -1,4 +1,7 @@
 <?php
+if(!isset($_SESSION['user'])){
+    header('Location: login.php');
+}
 $rconHost = getenv('RCON_HOST');
 $rconPort = getenv('RCON_PORT');
 $rconPassword = getenv('RCON_PASSWORD');
